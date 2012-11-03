0f39d006e2041712014fb8341f2a6a9db6dbf77d
Edward Cho <zerokarmaleft@gmail.com>
2012-11-03 17:46:36 -0500

    add examples

--- src/similarity/core.clj
+++ src/similarity/core.clj
@@ -4,9 +4,6 @@
             [clojure.string :as str])
   (:use swank.core))
 
-(def gettysburg-address
-  "Four score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal.\nNow we are engaged in a great civil war, testing whether that nation, or any nation, so conceived and so dedicated, can long endure. We are met on a great battle-field of that war. We have come to dedicate a portion of that field, as a final resting place for those who here gave their lives that that nation might live. It is altogether fitting and proper that we should do this.\nBut, in a larger sense, we can not dedicate, we can not consecrate, we can not hallow this ground. The brave men, living and dead, who struggled here, have consecrated it, far above our poor power to add or detract. The world will little note, nor long remember what we say here, but it can never forget what they did here. It is for us the living, rather, to be dedicated to the great task remaining before us--that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion--that we here highly resolve that these dead shall not have died in vain--that this nation, under God, shall have a new birth of freedom--and that government of the people, by the people, for the people, shall not perish from the earth.")
-
 (def U  #{\a \b \c \d \e})
 (def S1 #{\a \d})
 (def S2 #{\c})
@@ -15,10 +12,22 @@
 (def docs [S1 S2 S3 S4])
 
 (defn sim-jaccard
-  [s t]
   "Returns the similarity of sets as a relative size of their intersection."
-  (/ (count (set/intersection s t))
-     (count (set/union s t))))
+  ([s t]
+     (/ (count (set/intersection s t))
+        (count (set/union s t))))
+  ([s t & args]
+     (let [sets (conj (conj args t) s)]
+       (/ (count (apply set/intersection sets))
+          (count (apply set/union sets))))))
+
+(comment ; Exercise 3.1.1
+  (sim-jaccard #{1 2 3 4} #{1 2 3 4})
+  (sim-jaccard #{1 2 3 4} #{2 3 5 7})
+  (sim-jaccard #{1 2 3 4} #{2 4 6})
+  (sim-jaccard #{2 3 5 7} #{2 3 5 7})
+  (sim-jaccard #{2 3 5 7} #{2 4 6})
+  (sim-jaccard #{2 4 6} #{2 4 6}))
 
 (defn shingles
   [k s]
@@ -26,8 +35,19 @@
   (->> (str/replace s #"\s+" " ")
        (partition k 1 s)
        (map #(apply str %))
-       (map #(.hashCode %))
        (apply sorted-set)))
+
+(comment ; Example 3.3
+  (shingles 2 "abcdabd"))
+
+(comment ; Example 3.4
+  (let [D1 (shingles 9 "The plane was ready for touch down")
+        D2 (shingles 9 "The quarterback scored a touchdown")]
+    (sim-jaccard D1 D2))))
+
+(comment ; Exercise 3.2 1
+    (let [D "The most effective way to represent documents as sets, for the purpose of identifying lexically similar documents is to construct from the document the set of short strings that appear within it."]
+      (take 10 (shingles 3 D))))
 
 (defn characteristics
   [coll]
@@ -77,3 +97,5 @@
 ;; (reduce merge-sigs (vec (cycle c nil)) sig-partials)
 (comment
   (signatures docs hash-1 hash-2))
+
+(defn read)
